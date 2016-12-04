d1e7f7b68888084bd6a073b9d52cae527e61c46b
moerwald <andreas.mewald@gmail.com>
2016-12-04 18:38:52 +0000

    pubnub channel can now be set as command line parameter

--- src/pubnub_temp_hum.py
+++ src/pubnub_temp_hum.py
@@ -6,13 +6,15 @@ import Adafruit_DHT
 import time
 import pdb
 from pubnub import Pubnub
+import time
 
 
 class Room:
-   def __init__ (self, name, temp, humididy):
+   def __init__ (self, name, temp, humididy, datetime):
        self.Name = name
        self.Temperature = temp
        self.Humidity = humididy
+       self.DateTime = datetime
 
 def signal_handler(signal, frame):
     print('You pressed Ctrl+C!')
@@ -38,23 +40,31 @@ def disconnect(message):
     print("DISCONNECTED")
   
 #main
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) < 2:
+   print "Number of args is wrong. Going to exit"
+   exit (-1)
+
 global stopScript
 stopScript = False
 signal.signal(signal.SIGINT, signal_handler)
 
 pubnub = Pubnub(publish_key="pub-c-55470b0a-fe27-4763-b774-3aa63111fee0", subscribe_key="sub-c-783f7138-b7bd-11e6-91e2-02ee2ddab7fe")
-channelName='home_channel'
+channelName=sys.argv[1]
 
 time.sleep(10)
 
 while True:
    humdity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
    humdity -= 7
-   room = Room('bedroom', round(temperature,2), round(humdity,2))
+   now = time.strftime("%d/%m/%Y-%H:%M:%S")
+   room = Room('bedroom', round(temperature,2), round(humdity,2), now)
    pubnub.publish(channel=channelName,  message=room.__dict__)
    print ("Send message to pubnub")
    print (room.__dict__)
-   time.sleep(300)
+   time.sleep(3)
    if stopScript:
       break
 
